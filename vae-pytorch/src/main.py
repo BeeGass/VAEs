d7ef3c189467a6aa45465ab8994e05d5e5e5d85a
Bryan Gass <bryank123@live.com>
2021-12-07 06:24:45 -0500

    wanted to clean up the main file a bit and put a bit more into data_process

--- vae-pytorch/src/main.py
+++ vae-pytorch/src/main.py
@@ -9,51 +9,8 @@ from vae import VAE
 from loss import Losses
 from train import train
 from test import test
+from data_process import prepare_datasets, load_datasets
 
-
-def prepare_datasets():
-    # Transforms images to a PyTorch Tensor
-    tensor_transform = transforms.ToTensor()
-    
-    # Download the MNIST Dataset
-    trainset = datasets.MNIST(
-        root = './vae-pytorch/data',
-        train = True, 
-        download = True,
-        transform = tensor_transform
-    )
-    
-    testset = datasets.MNIST(
-        root = './vae-pytorch/data',
-        train = False,
-        download = True,
-        transform = tensor_transform
-    )
-    
-    return trainset, testset
-
-def load_datasets(trainset, testset, batch_size, num_workers):
-    # DataLoader is used to load the dataset 
-    # for training
-    train_loader = torch.utils.data.DataLoader(
-        trainset, 
-        batch_size = batch_size,
-        shuffle = True,
-        pin_memory = True,
-        num_workers = num_workers
-    )
-    
-    # DataLoader is used to load the dataset 
-    # for testing
-    test_loader = torch.utils.data.DataLoader(
-        testset, 
-        batch_size = batch_size,
-        shuffle = True,
-        pin_memory = True,
-        num_workers = num_workers
-    )
-    
-    return train_loader, test_loader
   
 @hydra.main(config_path="conf", config_name="config")
 def config_run(cfg : DictConfig) -> None:
---
+++ vae-pytorch/src/processing/data_process.py
@@ -0,0 +1,47 @@
+import torch 
+from torchvision import datasets
+from torchvision import transforms
+
+def prepare_datasets():
+    # Transforms images to a PyTorch Tensor
+    tensor_transform = transforms.ToTensor()
+    
+    # Download the MNIST Dataset
+    trainset = datasets.MNIST(
+        root = './vae-pytorch/data',
+        train = True, 
+        download = True,
+        transform = tensor_transform
+    )
+    
+    testset = datasets.MNIST(
+        root = './vae-pytorch/data',
+        train = False,
+        download = True,
+        transform = tensor_transform
+    )
+    
+    return trainset, testset
+
+def load_datasets(trainset, testset, batch_size, num_workers):
+    # DataLoader is used to load the dataset 
+    # for training
+    train_loader = torch.utils.data.DataLoader(
+        trainset, 
+        batch_size = batch_size,
+        shuffle = True,
+        pin_memory = True,
+        num_workers = num_workers
+    )
+    
+    # DataLoader is used to load the dataset 
+    # for testing
+    test_loader = torch.utils.data.DataLoader(
+        testset, 
+        batch_size = batch_size,
+        shuffle = True,
+        pin_memory = True,
+        num_workers = num_workers
+    )
+    
+    return train_loader, test_loader
